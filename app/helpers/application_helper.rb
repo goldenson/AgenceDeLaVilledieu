c4078b3068f38bb8f51c7fc37a1047090a2d2218
Goldenson <orefice.maxime@gmail.com>
2014-11-13 22:45:15 +0700

    Function energy and impact done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,47 @@
 module ApplicationHelper
+
+	def get_energy_letter(energy_number)
+		n = energy_number
+		case n
+		when 0..50
+		  return "A"
+		when 51..90
+		  return "B"
+		when 91..150
+		  return "C"
+		when 151..230
+		  return "D"
+		when 231..330
+		  return "E"
+		when 331..450
+		  return "F"
+		when 451..999
+		  return "G"
+		else
+		  puts "Ce nombre : #{n} n'est pas possible !"
+		end
+	end
+
+	def get_impact_letter(impact_number)
+		n = impact_number
+		case n
+		when 0..4
+		  return "A"
+		when 5..10
+		  return "B"
+		when 11..20
+		  return "C"
+		when 21..35
+		  return "D"
+		when 36..55
+		  return "E"
+		when 56..80
+		  return "F"
+		when 81..999
+		  return "G"
+		else
+		  puts "Ce nombre : #{n} n'est pas possible !"
+		end
+	end
+
 end
