bc8eaa25b59a350137b4c7a6d29cfb7573baa358
Goldenson <orefice.maxime@gmail.com>
2014-11-16 15:14:24 +0100

    Refactoring dpe, adding size validation for picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -21,5 +21,7 @@ class Picture < ActiveRecord::Base
   has_attached_file :image,
   :styles => { :large => "760x670>", :medium => "300x300>", :thumb => "100x100>" },
   :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment :image, :presence => true,
+  :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
+  :size => { :in => 0..5000.kilobytes }
 end
